e2bef0e3021f312f87c115b19dc6405334e6908c
dkinzer <dtkinzer@gmail.com>
2012-12-20 17:05:13 -0500

    Generate tests for fields options.
    Also, do not generate automatic tests for hidden fields.
    (We may need to revisit this decision).

--- bin/form.js
+++ bin/form.js
@@ -27,10 +27,19 @@ Generate.FormTests = function(id, casper) {
   casper.echo('');
 
   for(var field_id in fields) {
-    var field_key = field_id.toCamel();
     var field = fields[field_id];
+    // Do not generate automatic tests for hidden fields.
+    if (field.type != 'hidden') {
+      Generate.FormTests.Fields(field, casper);
+    }
+  }
+};
 
-    casper.echo('fields.' + field_key);
+Generate.FormTests.Fields = function(field, casper) {
+    var field_id = field.field_id;
+
+    casper.echo('var ' + field_id + ' = fields.' + field_id  + ';' );
+    casper.echo(field_id);
     casper.echo('  .assertExists("The field - ' + field.name + ' is present.")');
     if (field.label) {
       casper.echo('  .assertLabel("' + field.label + '", "The field label is: ' + field.label + '")');
@@ -43,14 +52,19 @@ Generate.FormTests = function(id, casper) {
     }
     casper.echo('casper.echo(\'\');');
     casper.echo('');
-  }
 };
-
 
 Generate.FormTests.Selectors = function(field, casper) {
+  var field_id = field.field_id;
+  casper.echo('');
   for (var option in field.options) {
-    casper.echo('option.' + option);
+    var value = field.options[option].value;
+    var label = field.options[option].label;
+    var fieldText = field_id + '.options.' + option;
+    var assertValueText = fieldText + '.assertValue("' + value + '", "The option value is: ' + value + '")';
+    var assertLabelText = ' .assertLabel("' + label + '", "The option label  is: ' + label + '");';
+    casper.echo(assertValueText);
+    casper.echo(assertLabelText);
   }
 };
-
 
