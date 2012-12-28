f0e4b319a119f236923ca18ba84ed186b2da1a07
dkinzer <dtkinzer@gmail.com>
2012-12-28 11:15:01 -0500

    Fix ecto.sh not working via symlink.
    Also add selftest mechanism. `ecto selftest` now runs the default ecto
    tests.

--- bin/ecto.sh
+++ bin/ecto.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # @file
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+setSourceDirectory()
+{
+  scriptPath=$(which $0)
 
-casperjs --url="$1" --id='node-form' --ignore-ssl-errors='yes' --casperIncludes="$DIR/../casper-includes" "$DIR/../src/form.js"
+  if [ -L $scriptPath ]; then
+
+    DIR=$(dirname $(readlink -f $scriptPath))
+  else
+    DIR=$(dirname $scriptPath)
+  fi
+}
+
+setSourceDirectory
+
+INCLUDES="$DIR/../casper-includes"
+
+if [ $1 = "selftest" ]
+then
+  casperjs test --testSlow='yes' --ignore-ssl-errors='yes' --casperIncludes="$INCLUDES" --includes="$INCLUDES/Eval.js" "$INCLUDES/tests/Eval.test.js"
+else 
+  echo $1
+  casperjs --url="$1" --id='node-form' --ignore-ssl-errors='yes' --casperIncludes="$DIR/../casper-includes" "$DIR/../src/form.js"
+fi
+
